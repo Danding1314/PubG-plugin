f7d2dc442358d6de62b963dc01daba565a6c8c74
Shadow-87 <72851703+Shadow-87@users.noreply.github.com>
2020-10-19 13:20:53 +0330

    Create Vector3.php

---
+++ src/shadowminer/pubg/math/Vector3.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace shadowminer\pubg\math;
+
+/**
+ * Class Vector3
+ * @package pubg\math
+ */
+class Vector3 extends \pocketmine\math\Vector3 {
+
+    /**
+     * @return string
+     */
+    public function __toString() {
+        return "$this->x,$this->y,$this->z";
+    }
+
+    /**
+     * @param string $string
+     * @return Vector3
+     */
+    public static function fromString(string $string) {
+        return new Vector3((int)explode(",", $string)[0], (int)explode(",", $string)[1], (int)explode(",", $string)[2]);
+    }
+}
