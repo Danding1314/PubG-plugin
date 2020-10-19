1c43637aa417131d290f9ac754fc84b412001df7
Shadow-87 <72851703+Shadow-87@users.noreply.github.com>
2020-10-19 12:55:24 +0330

    Update PubG.php

--- src/shadowminer/pubg/PubG.php
+++ src/shadowminer/pubg/PubG.php
@@ -1,20 +1,5 @@
 <?php
 
-/**
- * Copyright 2018-2020 GamakCZ
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 declare(strict_types=1);
 
@@ -32,10 +17,7 @@ use ShadowMiner\pubg\commands\pubgCommand;
 use ShadowMiner\pubg\math\Vector3;
 use ShadowMiner\pubg\provider\YamlDataProvider;
 
-/**
- * Class SkyWars
- * @package skywars
- */
+
 class PubG extends PluginBase implements Listener {
 
     /** @var YamlDataProvider */
